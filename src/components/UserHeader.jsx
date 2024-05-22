522e1c82d352bca7450922e50e5f8169e2a977aa
roblebob <robert.koppisch@gmail.com>
2024-05-22 14:30:56 +0200

    @42:36

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -10,11 +10,25 @@ import {
   Portal,
   Text,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
 
 const UserHeader = () => {
+  const toast = useToast();
+
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() =>
+      toast({
+        status: "success",
+        description: "URL copied to clipboard",
+        duration: 2000,
+        isClosable: true,
+      })
+    );
+  };
   return (
     <VStack gap={4} alignItems={"start"}>
       <Flex justifyContent={"space-between"} w={"full"}>
@@ -53,18 +67,31 @@ const UserHeader = () => {
           <Box className="icon-container">
             <BsInstagram size={24} cursor={"pointer"} />
           </Box>
+
           <Box className="icon-container">
             <Menu>
               <MenuButton>
                 <CgMoreO size={24} cursor={"pointer"} />
               </MenuButton>
               <Portal>
-                <MenuList>
-                  <MenuItem>Copy Link</MenuItem>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                    Copy Link
+                  </MenuItem>
                 </MenuList>
               </Portal>
             </Menu>
           </Box>
+        </Flex>
+      </Flex>
+
+      <Flex w={"full"}>
+        <Flex flex={1} borderButton={"1.5px solid white"} justifyContent={"center"} pb="3" cursor={"pointer"}>
+          <Text fontWeight={"bold"}>Threads</Text>
+        </Flex>
+
+        <Flex flex={1} borderButton={"1.5px solid white"} justifyContent={"center"} pb="3" cursor={"pointer"}>
+          <Text fontWeight={"bold"}>Replies</Text>
         </Flex>
       </Flex>
     </VStack>
