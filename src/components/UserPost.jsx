f11bc0f7cfdb95cbd96f8ead6f733c7e5cbde747
roblebob <robert.koppisch@gmail.com>
2024-05-23 10:44:42 +0200

    @1:05:51

--- src/components/UserPost.jsx
+++ src/components/UserPost.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Actions from "./Actions";
 import { useState } from "react";
 
-const UserPost = () => {
+const UserPost = ({ likes, replies, postImage, postTitle }) => {
   const [liked, setLiked] = useState(false);
 
   return (
@@ -61,24 +61,31 @@ const UserPost = () => {
             </Flex>
           </Flex>
 
-          <Text fontSize={"sm"}>This is my first post</Text>
-          <Box
-            borderRadius={6}
-            overflow={"hidden"}
-            border={"1px solid"}
-            borderColor={"gray.light"}
-          >
-            <Image src="/post1.png" w={"full"} />
-          </Box>
+          <Text fontSize={"sm"}>{postTitle}</Text>
+
+          {postImage && (
+            <Box
+              borderRadius={6}
+              overflow={"hidden"}
+              border={"1px solid"}
+              borderColor={"gray.light"}
+            >
+              <Image src={postImage} w={"full"} />
+            </Box>
+          )}
 
           <Flex gap={3} my={1}>
             <Actions liked={liked} setLiked={setLiked} />
           </Flex>
 
           <Flex gap={2} alignItems={"center"}>
-            <Text color={'gray.light'} fontSize="sm">123 replies</Text>
-            <Box w={0.5} h={0.5} borderRadius={"full"} bg={'gray.light'} />
-            <Text color={'gray.light'} fontSize="sm">456 likes</Text>
+            <Text color={"gray.light"} fontSize="sm">
+              {replies} replies
+            </Text>
+            <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"} />
+            <Text color={"gray.light"} fontSize="sm">
+              {likes} likes
+            </Text>
           </Flex>
         </Flex>
       </Flex>
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -4,7 +4,10 @@ import UserPost from "../components/UserPost";
 const UserPage = () => {
   return <>
    <UserHeader />
-   <UserPost />
+   <UserPost likes={1200} replies={401} postImage={"/post1.png"} postTitle={"Let'S talk"}/>
+   <UserPost likes={450} replies={41} postImage={"/post2.png"} postTitle={"Let'S talk"}/>
+   <UserPost likes={200} replies={50} postImage={"/post3.png"} postTitle={"Let'S talk"}/>
+   <UserPost likes={100} replies={101}  postTitle={"Let'S talk"}/>
   </>;
 };
 
